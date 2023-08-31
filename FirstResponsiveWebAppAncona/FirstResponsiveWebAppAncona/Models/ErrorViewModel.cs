0093cd52f68c6cdaecd0b8c168f1d954b490bcd4
Tim Ancona <tsancona@gmail.com>
2023-08-30 21:24:45 -0500

    added First Responsive Web App assignment

---
+++ FirstResponsiveWebAppAncona/FirstResponsiveWebAppAncona/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstResponsiveWebAppAncona.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
