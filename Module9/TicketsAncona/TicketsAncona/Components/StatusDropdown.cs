66eed081350fce28e070ff8f83a9da10611f8930
Tim Ancona <tsancona@gmail.com>
2023-11-29 23:48:38 -0600

    added tag helper for submit buttons and view component for status dropdown on Add page

---
+++ Module9/TicketsAncona/TicketsAncona/Components/StatusDropdown.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketsAncona.Models;
+
+namespace TicketsAncona.Components
+{
+    public class StatusDropdown : ViewComponent
+    {
+        public IViewComponentResult Invoke(TicketViewModel model)
+        {
+            return View("~/Views/Shared/_StatusDropdownPartial.cshtml", model);
+        }
+    }
+}
---
+++ Module9/TicketsAncona/TicketsAncona/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TicketsAncona.TagHelpers
+{
+    public class ButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string newClasses = "btn btn-primary";
+            string oldClasses = output.Attributes["class"]?.Value.ToString() ?? "";
+            string classes = (string.IsNullOrEmpty(oldClasses)) ? newClasses : $"{oldClasses} {newClasses}";
+            output.Attributes.SetAttribute("class", classes);
+        }
+    }
+}
